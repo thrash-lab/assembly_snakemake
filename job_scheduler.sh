c33e5a9bb572465beb20231979798378569c3a8b
Conner Kojima <connerkojima@usc-securewireless-student-new12.usc.edu>
2022-09-11 17:13:05 -0700

    setting the number of openblas threads equal to one

--- job_scheduler.sh
+++ job_scheduler.sh
@@ -21,4 +21,11 @@ set -o errexit
 set -x
 
 ### run your commands here!
-snakemake /project/thrash_89/db/EAGER_metaG_for_ck/pipeline_assemblies/ARD_V9_S7_assembly_dir/checkm/dastools/output_table.txt --profile .config/slurm/
+snakemake /project/thrash_89/db/EAGER_metaG_for_ck/pipeline_assemblies/CJ_V3_S9_assembly_dir/checkm/dastools/output_table.txt --profile .config/slurm/
+
+# ARDs done, 
+# ARD_V2_S1*  ARD_V7_S5*   CJ_V2_S8*   CJ_V7_S12    LKB_V4_S16  LKB_V8_S20    Tbon_V5_S24  Undetermined_S0
+# ARD_V3_S2*  ARD_V8_S6*   CJ_V3_S9   CJ_V8_S13    LKB_V5_S17  Tbon_V10_S28  Tbon_V6_S25
+# ARD_V5_S3*  ARD_V9_S7*   CJ_V4_S10  LKB_V10_S21  LKB_V6_S18  Tbon_V2_S22   Tbon_V7_S26
+# ARD_V6_S4*  CJ_V10_S14*  CJ_V5_S11  LKB_V2_S15   LKB_V7_S19  Tbon_V4_S23   Tbon_V8_S27
+
