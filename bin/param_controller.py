a6b6150012d18c8338b14e63f26a689a2067ef2c
Conner Kojima <connerkojima@Conners-MacBook-Pro.local>
2022-11-04 22:59:04 -0700

    configuring time values for 1% subassembly

--- bin/param_controller.py
+++ bin/param_controller.py
@@ -15,7 +15,10 @@ Here's a better idea. I make a separate test code environment where I test the s
 for 1, 5, 10, 20, and 50 on a single sample. I guess and check the time constraints until they are done
 
 TODO: 
-make separate framework to test time limits
+make separate framework to test time limits (done)
+make functions in param_controller2 to have different time limits (IP)
+test subassemblies one by one (e.g. --sub 1) on hpc
+adjust number accordingly
 
 
 
@@ -38,3 +41,60 @@ def calculate_sickle_trim_full_mem(wildcards):
 def calculate_sickle_trim_full_partition(wildcards):
     config = get_yaml_fields()
     return config['slurm_resources']['sickle_trim_full']['partition']
+
+#
+def calculate_m_assembly_time(wildcards):
+    config = get_yaml_fields()
+    return config['slurm_resources']['metaspades_assembly']['time'][str("{0}_per".format(wildcards.subset[:-7]))]
+
+def calculate_m_assembly_mem(wildcards):
+    config = get_yaml_fields()
+    return config['slurm_resources']['metaspades_assembly']['mem']
+
+def calculate_m_assembly_partition(wildcards):
+    config = get_yaml_fields()
+    return config['slurm_resources']['metaspades_assembly']['partition']
+
+def calculate_bowtie2_time(wildcards):
+    config = get_yaml_fields()
+    return config['slurm_resources']['bowtie2_map_reads']['time'][str("{0}_per".format(wildcards.subset[:-7]))]
+
+def calculate_g_depth_time(wildcards):
+    config = get_yaml_fields()
+    return config['slurm_resources']['generate_depth_files']['time'][str("{0}_per".format(wildcards.subset[:-7]))]
+
+def calculate_metabat2_time(wildcards):
+    config = get_yaml_fields()
+    return config['slurm_resources']['bin_metabat2']['time'][str("{0}_per".format(wildcards.subset[:-7]))]
+
+def calculate_concoct_time(wildcards):
+    config = get_yaml_fields()
+    return config['slurm_resources']['bin_concoct']['time'][str("{0}_per".format(wildcards.subset[:-7]))]
+
+def calculate_maxbin_time(wildcards):
+    config = get_yaml_fields()
+    return config['slurm_resources']['bin_maxbin2']['time'][str("{0}_per".format(wildcards.subset[:-7]))]
+
+def calculate_dastools_time(wildcards):
+    config = get_yaml_fields()
+    return config['slurm_resources']['generate_consensus_bins_dastools']['time'][str("{0}_per".format(wildcards.subset[:-7]))]
+
+def calculate_checkm_time(wildcards):
+    config = get_yaml_fields()
+    return config['slurm_resources']['evaluate_bins_checkm']['time']
+
+def calculate_subset_par_time(wildcards):
+    config = get_yaml_fields()
+    return config['slurm_resources']['subset_reads_par']['time']
+
+def calculate_map_subassembly_time(wildcards):
+    config = get_yaml_fields()
+    return config['slurm_resources']['map_subassembly_reads']['time']
+
+def calculate_convert_bam_time(wildcards):
+    config = get_yaml_fields()
+    return config['slurm_resources']['convert_bam_to_fastq']['time']
+
+def subset_reads_ser(wildcards):
+    config = get_yaml_fields()
+    return config['slurm_resources']['subset_reads_ser']['time']
