694bf780a8d09739b1883a45dfdd047752144396
Conner Kojima <connerkojima@Conners-MacBook-Pro.local>
2022-11-27 15:06:38 -0800

    adding to samples list

--- bin/test_analyze_checkm.py
+++ bin/test_analyze_checkm.py
@@ -4,12 +4,27 @@ import numpy as np
 import csv
 
 
-dict = {"Bin Id": [], "Marker lineage": [], "# genomes": [], "# markers": [], "# marker sets": [], "completeness": [], "contamination": [], "Strain heterogeneity": []}
+dict = {"Bin Id": [], 
+        "Marker lineage": [], 
+        "# genomes": [], 
+        "# markers": [], 
+        "# marker sets": [], 
+        "0": [],
+        "1": [],
+        "2": [],
+        "3": [],
+        "4": [],
+        "5+": [],
+        "completeness": [], 
+        "contamination": [], 
+        "Strain heterogeneity": [], 
+        "quality_metric": []}
+
 counth = 0
 countl = 0
 countm = 0
 countx = 0
-for file in os.listdir("/Users/connerkojima/thrash/checkm_outputs"):
+for file in os.listdir("/Users/connerkojima/thrash/checkm_outputs/"):
     with open("/Users/connerkojima/thrash/checkm_outputs/{}".format(file)) as f:
         lines = f.readlines()
         lines = lines[3:-1]
@@ -17,13 +32,20 @@ for file in os.listdir("/Users/connerkojima/thrash/checkm_outputs"):
         for line in lines:
             tokens = line.split()
             dict["Bin Id"].append(tokens[0])
-            dict["Marker lineage"].append(tokens[1])
+            dict["Marker lineage"].append("{} {}".format(tokens[1], tokens[2]))
             dict["# genomes"].append(tokens[3])
             dict["# markers"].append(tokens[4])
             dict["# marker sets"].append(tokens[5])
-            dict["completeness"].append(tokens[12])
-            dict["contamination"].append(tokens[13])
-            dict["Strain heterogeneity"].append(tokens[14])
+            dict["0"].append(tokens[6])
+            dict["1"].append(tokens[7])
+            dict["2"].append(tokens[8])
+            dict["3"].append(tokens[9])
+            dict["4"].append(tokens[10])
+            dict["5+"].append(tokens[11])
+            dict["completeness"].append(str(tokens[12]))
+            dict["contamination"].append(str(tokens[13]))
+            dict["Strain heterogeneity"].append(str(tokens[14]))
+            dict["quality_metric"].append(float(tokens[12])-float(tokens[13])*5)
             if float(tokens[12]) > 90 and float(tokens[13]) < 5:
                 counth = counth + 1
             if float(tokens[12]) >= 50 and float(tokens[13]) < 10:
@@ -35,7 +57,11 @@ for file in os.listdir("/Users/connerkojima/thrash/checkm_outputs"):
             
 
 data = pd.DataFrame(dict)
-print(counth)
-print(countm)
-print(countl)
-print(countx)
+data_10 = data.nlargest(20, "quality_metric")
+
+data_10.to_csv("/Users/connerkojima/thrash/20_best.csv")
+
+# print(counth)
+# print(countm)
+# print(countl)
+# print(countx)
